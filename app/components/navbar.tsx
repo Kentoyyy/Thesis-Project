b97efe5649272e34ccc58f319ebffb75b2d92511
_Kentoyyy <kenalcantara55@gmail.com>
2024-08-19 13:27:17 +0800

    updating

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -18,15 +18,7 @@ const Navbar: React.FC = () => {
           <Link href="/login" className="text-base text-sub-color hover:text-gray-400">Login</Link>
         </div>
         <button className="md:hidden text-black ml-auto">
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
+          
         </button>
       </div>
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import Navbar from './components/navbar';
 
-export default function page() {
+export default function Home() {
   return (
     <>
     <Navbar />
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Welcome to Next.js!</h1>
-      <p>This is the homepage.</p>
-    </div>
+    
   </>
   )
 }
