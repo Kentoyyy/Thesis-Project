aec7a3482e2340347ccc560cd58691e0bf6812b5
_Kentoyyy <kenalcantara55@gmail.com>
2024-08-23 21:34:33 +0800

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-white text-gray-600 py-3 border-t">
-      <div className="container mx-auto text-center">
-        <p className="text-xs">
+      <div className="max-w-screen-xl mx-auto px-4 text-center flex flex-col items-center md:flex-row md:justify-between md:items-center">
+        <p className="text-xs sm:text-sm">
           &copy; {new Date().getFullYear()} ECLD. All rights reserved.
         </p>
       </div>
